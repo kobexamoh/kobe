f16b7f425526d39fb1ab066e9a4b79a4116c227f
Kobe Amoh <amoh@ualberta.ca>
2019-10-07 20:42:45 -0600

    Hamburger menu creation
    Created the hamburger menu toggles. Next up: creating the clickable elements for the toggle.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 // variable declaration
-var sample = document.querySelector('h2');
+let hamburgerMenu = document.querySelector('#hamburger');
+let cancelMenu = document.querySelector("#close"); // todo: think of a better name for this variable
 
-// event listener
-sample.addEventListener(ondblclick, doTheThing);
+// event listener(s)
+hamburgerMenu.addEventListener('click', openMenu);
+cancelMenu.addEventListener('click', closeMenu);
 
-// function
-function doTheThing(e){
-    console.log('Working!');
+// function(s)
+function openMenu(evt) {
+    
+    // pseudocode for the menu is as follows:
+    // when the hamburger menu is tapped, it should
+    // become the 'X' menu. That is, it should disappear,
+    // and the 'X' should appear.
+    hamburgerMenu.classList.add('hidden');
+    cancelMenu.classList.remove('hidden');
+    evt.preventDefault();
 }
+
+function closeMenu(evt) {
+    // pseudocode is as follows:
+    // when the 'X' is tapped, it should disappear
+    // and the hamburger menu should reappear
+    cancelMenu.classList.add('hidden');
+    hamburgerMenu.classList.remove('hidden');
+    evt.preventDefault();
+}
+
+
